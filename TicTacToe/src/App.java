14d5f5134df8b1f8eb410869798725ee72bcd27c
Looper <aayush.bhattacharjee2002@gmail.com>
2021-11-21 04:05:46 -0800

    started working on the networking of the app

--- TicTacToe/src/App.java
+++ TicTacToe/src/App.java
@@ -1,5 +1,26 @@
+import java.io.*;
+import java.net.*;
+
 public class App {
+    public static ServerSocket server;
+    public static DataInputStream inputStream;
+    public static DataOutputStream outputStream;
+
+    public static void start(int port) throws Exception {
+        server = new ServerSocket(port);
+        server.setSoTimeout(600000);
+        Socket socket = server.accept();
+        if(socket.isConnected()) {
+            System.out.println("Connected");
+            inputStream = new DataInputStream(socket.getInputStream());
+            outputStream = new DataOutputStream(socket.getOutputStream());
+        }
+        else{
+            System.out.println("Failed to connect");
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        
+
     }
 }
