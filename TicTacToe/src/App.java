867c0720e64d96e5c763b745a021dcad7d235639
Looper <aayush.bhattacharjee2002@gmail.com>
2021-12-04 11:21:03 -0800

    made the gameBoard

--- TicTacToe/src/App.java
+++ TicTacToe/src/App.java
@@ -1,31 +1,33 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-class networking{
+
+class networking {
     public ServerSocket server;
     DataInputStream inputStream;
     DataOutputStream outputStream;
+
     public void start(int port) throws Exception {
-        try{
+        try {
             server = new ServerSocket(port);
             Socket socket = server.accept();
-            if(socket.isConnected()) {
+            if (socket.isConnected()) {
                 System.out.println("Connected");
                 inputStream = new DataInputStream(socket.getInputStream());
                 outputStream = new DataOutputStream(socket.getOutputStream());
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Failed to connect");
         }
-       
+
     }
 }
 
-class game{
+class game {
     public String p1;
     public String p2;
-    game(String p1) throws Exception{
+
+    game(String p1) throws Exception {
         networking net = new networking();
         net.start(8008);
         this.p1 = p1;
@@ -34,20 +36,31 @@ class game{
         net.outputStream.flush();
 
     }
-    public void run() throws Exception{
-        System.out.println("Player 1: " + p1+"-->You");
+
+    public void run() throws Exception {
+        System.out.println("Player 1: " + p1 + "-->You");
         System.out.println("Player 2: " + p2);
         System.out.println("Press any button to continue");
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();  
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+        int gameBoard[][] = { { 0, 0, 0 },
+                            { 0, 0, 0 },
+                            { 0, 0, 0 } };
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard.length; j++) {
+                System.out.printf(" %d ", gameBoard[i][j]);
+            }
+            System.out.println();
+        }
+
     }
 }
+
 public class App {
-   
-   
+
     public static void main(String[] args) throws Exception {
         networking net = new networking();
         game game = new game("aayush");
         game.run();
-
     }
 }
